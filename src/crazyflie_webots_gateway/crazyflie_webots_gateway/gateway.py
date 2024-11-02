4dbc25b6a106320465ec2da90be484260bfcb2f0
vinzenzm <vinzenz@malke.info>
2024-11-02 20:46:32 +0100

    improved gateway error handling and coding style

--- src/crazyflie_webots_gateway/crazyflie_webots_gateway/gateway.py
+++ src/crazyflie_webots_gateway/crazyflie_webots_gateway/gateway.py
@@ -3,15 +3,18 @@ import os
 
 import rclpy
 from rclpy.node import Node
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 
 from crazyflie_webots_gateway_interfaces.srv import WebotsCrazyflie
 from ament_index_python.packages import get_package_share_directory, get_package_prefix
-from ros2run.api import get_executable_path
 
 from launch.substitutions import EnvironmentVariable
 from launch import LaunchContext
 
-from signal import SIGINT, SIGKILL
+from signal import SIGINT
+from typing import Dict
+
+from asyncio.subprocess import Process
 
 
 class Gateway(Node):
@@ -20,79 +23,131 @@ class Gateway(Node):
             "crazyflie_webots_gateway",
             automatically_declare_parameters_from_overrides=True,
         )
+        self._load_paths_and_directories()
+        self._logger = self.get_logger()
 
-        self.add_service = self.create_service(
-            WebotsCrazyflie, "~/add_crazyflie", self.add_crazyflie
+        self._logger.info(
+            "Started Webots-Gateway with "
+            + "IP: {} and PORT: {}. ".format(self._webots_ip, self._webots_port)
+            + "Change this configuration by setting eviromental variable"
+            + "'WEBOTS_IP' or 'WEBOTS_PORT' respectively. "
+            + "(e.g. 'export WEBTOS_IP=192.168.56.1')"
         )
-        self.remove_service = self.create_service(
-            WebotsCrazyflie, "~/remove_crazyflie", self.remove_crazyflie
+
+        self.crazyflies: Dict[int, Process] = {}
+
+        callback_group = MutuallyExclusiveCallbackGroup()
+        self.create_service(
+            WebotsCrazyflie,
+            "~/add_crazyflie",
+            self._add_crazyflie_callback,
+            callback_group=callback_group,
         )
 
-        self.cfs = {}
-
-    def add_crazyflie(self, req, resp):
-        self.get_logger().info("Got called to add crazyflie with ID: {}".format(req.id))
-        initial_position = [
-            req.initial_position.x,
-            req.initial_position.y,
-            req.initial_position.z,
-        ]
-        asyncio.ensure_future(self.create_cf(req.id))
-        resp.success = True
-        return resp
-
-    def remove_crazyflie(self, req, resp):
-        self.get_logger().info(
-            "Got called to remove Crazyflie with ID: {}".format(req.id)
+        self.create_service(
+            WebotsCrazyflie,
+            "~/remove_crazyflie",
+            self._remove_crazyflie_callback,
+            callback_group=callback_group,
         )
-        process = self.cfs[req.id]
-        os.killpg(os.getpgid(process.pid), SIGINT)
-        resp.success = True
-        return resp
 
-    async def create_cf(self, id):
-        webots_controller_path = (
+    def remove_crazyflie(self, id: int) -> bool:
+        self._logger.info("Removing crazyflie with ID: {}".format(id))
+        if id in self.crazyflies.keys():
+            process = self.crazyflies[id]
+            os.killpg(os.getpgid(process.pid), SIGINT)
+            return True
+
+        self._logger.info(
+            "Couldn't remove crazyflie with ID: {}; was not active. ".format(id)
+        )
+        return False
+
+    def remove_all_crazyflies(self) -> None:
+        for cf_key in self.crazyflies.keys():
+            _ = self.remove_crazyflie(cf_key)
+
+    def add_crazyflie(self, id: int) -> bool:
+        self._logger.info("Adding crazyflie with ID: {}".format(id))
+        asyncio.ensure_future(self._create_cf(id))
+        return True
+
+    async def _create_cf(self, id: int):
+        cmd = self._create_start_command(id)
+        self.crazyflies[id] = await asyncio.create_subprocess_shell(
+            cmd, preexec_fn=os.setsid
+        )
+        await self.crazyflies[id].wait()
+
+    def _create_start_command(self, id: int) -> str:
+        robot_name = "cf{}_ros_ctrl".format(id)
+
+        return (
+            "WEBOTS_HOME={} ".format(self._webots_home)
+            + self._webots_controller_path
+            + " --robot-name={}".format(robot_name)
+            + " --protocol=tcp"
+            + " --ip-address={}".format(self._webots_ip)
+            + " --port={}".format(self._webots_port)
+            + " ros2 --ros-args -p"
+            + " robot_description:={}".format(self._cf_description_path)
+        )
+
+    def _load_paths_and_directories(self):
+        self._webots_controller_path = (
             get_package_share_directory("webots_ros2_driver")
             + "/scripts/webots-controller"
         )
-        description_path = os.path.join(
+
+        self._cf_description_path = os.path.join(
             get_package_share_directory("crazyflie_webots"), "resource", "cf.urdf"
         )
-        webots_ip = EnvironmentVariable(
-            "WEBOTS_IP", default_value="192.168.56.1"
+
+        self._webots_ip = EnvironmentVariable(
+            "WEBOTS_IP", default_value="127.0.0.1"
         ).perform(LaunchContext())
-        webots_home = get_package_prefix("webots_ros2_driver")
 
-        robot_name = "cf{}_ros_ctrl".format(id)
+        self._webots_port = EnvironmentVariable(
+            "WEBOTS_PORT", default_value="1234"
+        ).perform(LaunchContext())
 
-        cmd = (
-            "WEBOTS_HOME={} ".format(webots_home)
-            + webots_controller_path
-            + " --robot-name="
-            + robot_name
-            + " --protocol=tcp --ip-address="
-            + str(webots_ip)
-            + " --port=1234 ros2 --ros-args -p robot_description:="
-            + description_path
-        )
-        self.get_logger().info(cmd)
-        self.cfs[id] = await asyncio.create_subprocess_shell(cmd, preexec_fn=os.setsid)
-        await self.cfs[id].wait()
+        self._webots_home = get_package_prefix("webots_ros2_driver")
+
+    def _add_crazyflie_callback(
+        self, req: WebotsCrazyflie.Request, resp: WebotsCrazyflie.Response
+    ) -> WebotsCrazyflie.Response:
+        resp.success = self.add_crazyflie(req.id)
+        return resp
+
+    def _remove_crazyflie_callback(
+        self, req: WebotsCrazyflie.Request, resp: WebotsCrazyflie.Response
+    ) -> WebotsCrazyflie.Response:
+        resp.success = self.remove_crazyflie(req.id)
+        return resp
 
 
 async def run_node():
+    rclpy.init()
     gateway = Gateway()
-    while rclpy.ok():
-        await asyncio.sleep(0.01)
-        rclpy.spin_once(gateway, timeout_sec=0)
-    rclpy.shutdown()
+    try:
+        while rclpy.ok():
+            await asyncio.sleep(0.01)
+            rclpy.spin_once(gateway, timeout_sec=0)
+        rclpy.shutdown()
+    except asyncio.CancelledError:
+        gateway.remove_all_crazyflies()
 
 
 def main():
-    rclpy.init()
     event_loop = asyncio.get_event_loop()
-    asyncio.ensure_future(run_node())
-    event_loop.run_forever()
+    node = asyncio.ensure_future(run_node())
+    try:
+        event_loop.run_forever()
+    except KeyboardInterrupt:
+        node.cancel()
+        event_loop.run_until_complete(node)
+    finally:
+        event_loop.close()
 
 
 if __name__ == "__main__":
