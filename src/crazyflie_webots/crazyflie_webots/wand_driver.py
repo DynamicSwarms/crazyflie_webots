2b32fbd80e2d62f0240f20e2df465826bbf4747e
vinzenzm <vinzenz@malke.info>
2025-01-31 12:29:14 +0100

    adapted hardware_gateway  improvements to the webots gateway. should launch and exit more cleanly

--- src/crazyflie_webots/crazyflie_webots/wand_driver.py
+++ src/crazyflie_webots/crazyflie_webots/wand_driver.py
@@ -1,26 +1,25 @@
-import rclpy
-
 from crazyflie_webots.wb_ros_driver import WebotsRosDriver
 from geometry_msgs.msg import Twist
 
+
 class WandDriverNode(WebotsRosDriver):
     def init(self, webots_node, properties):
-        super().init(webots_node, properties)        
-              
-        self.ros_node.create_subscription(Twist, 'cmd_vel', self.__cmd_vel_callback, 1)
-        
+        super().init(webots_node, properties)
+
+        self.ros_node.create_subscription(Twist, "cmd_vel", self.__cmd_vel_callback, 1)
+
     def __cmd_vel_callback(self, twist):
         self.__target_twist = twist
         x = twist.linear.x
         y = twist.linear.y
         z = twist.linear.z
-        #self.target_field.setSFVec3f([x, y, z])
+        # self.target_field.setSFVec3f([x, y, z])
 
     def step(self):
         super().step()
 
     # Overwrite because this is weird
     def getName(self):
-        field = self.wb_node.getField('wand_id')
+        field = self.wb_node.getField("wand_id")
         id = field.getSFInt32()
-        return "Wand0" + str(id)
+        return "Wand" + str(id)
--- src/crazyflie_webots_gateway/crazyflie_webots_gateway/gateway.py
+++ src/crazyflie_webots_gateway/crazyflie_webots_gateway/gateway.py
@@ -2,26 +2,46 @@ import os
 import asyncio
 from asyncio.subprocess import Process
 from signal import SIGINT
+import time
+from threading import Thread
 
 from ament_index_python.packages import get_package_share_directory, get_package_prefix
 from launch.substitutions import EnvironmentVariable
 from launch import LaunchContext
 
 import rclpy
+from rclpy import Future
 from rclpy.node import Node
+from rclpy.client import Client
+
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from lifecycle_msgs.msg import State as LifecycleState
+from lifecycle_msgs.srv import ChangeState, GetState
+
 
 from crazyflie_webots_gateway_interfaces.srv import WebotsCrazyflie
 
-from typing import Dict
+from dataclasses import dataclass
+from typing import Dict, Optional, Tuple
+
+
+class GatewayError(Exception):
+    pass
+
+
+@dataclass
+class CrazyflieInstance:
+    process: Optional[Process] = None
 
 
 class Gateway(Node):
-    def __init__(self):
+    def __init__(self, event_loop):
         super().__init__(
             "crazyflie_webots_gateway",
             automatically_declare_parameters_from_overrides=True,
         )
+
+        self._cf_event_loop = event_loop
         self._load_paths_and_directories()
         self._logger = self.get_logger()
 
@@ -32,8 +52,8 @@ class Gateway(Node):
             + "'WEBOTS_IP' or 'WEBOTS_PORT' respectively. "
             + "(e.g. 'export WEBTOS_IP=192.168.56.1')"
         )
-
-        self.crazyflies: Dict[int, Process] = {}
+        self.crazyflies: Dict[int, CrazyflieInstance] = {}
+        self.crazyflie_callback_group = MutuallyExclusiveCallbackGroup()
 
         callback_group = MutuallyExclusiveCallbackGroup()
         self.create_service(
@@ -50,33 +70,49 @@ class Gateway(Node):
             callback_group=callback_group,
         )
 
-    def remove_crazyflie(self, id: int) -> bool:
+    def remove_crazyflie(self, id: int) -> Tuple[bool, str]:
         self._logger.info("Removing crazyflie with ID: {}".format(id))
         if id in self.crazyflies.keys():
-            process = self.crazyflies[id]
+            process = self.crazyflies[id].process
             os.killpg(os.getpgid(process.pid), SIGINT)
-            return True
+            return True, "Success"
 
-        self._logger.info(
-            "Couldn't remove crazyflie with ID: {}; was not active. ".format(id)
-        )
-        return False
+        msg = "Couldn't remove crazyflie with ID: {}; was not active. ".format(id)
+        self._logger.info(msg)
+        return False, msg
 
     def remove_all_crazyflies(self) -> None:
-        for cf_key in self.crazyflies.keys():
+        for cf_key in list(self.crazyflies.keys()):
             _ = self.remove_crazyflie(cf_key)
+            del self.crazyflies[cf_key]
 
     def add_crazyflie(self, id: int) -> bool:
         self._logger.info("Adding crazyflie with ID: {}".format(id))
-        asyncio.ensure_future(self._create_cf(id))
-        return True
+        if id in self.crazyflies.keys():
+            raise GatewayError("Cannot add Crazyflie, is already in Gateway!")
+        else:
+            wait_future = asyncio.run_coroutine_threadsafe(
+                self._create_cf(id), loop=self._cf_event_loop
+            )
+            wait_future.add_done_callback(lambda fut: self._on_crazyflie_exit(fut, id))
+            success = True
+        return success
+
+    def _on_crazyflie_exit(self, fut: asyncio.Future, id: int):
+        """Callback invoked when a crazyflie subprocess exits."""
+        self._logger.info(f"Crazyflie (id={id}) exited.")
+
+        # Clean up the crazyflie entry from the dictionary
+        if id in self.crazyflies.keys():
+            del self.crazyflies[id]
 
     async def _create_cf(self, id: int):
         cmd = self._create_start_command(id)
-        self.crazyflies[id] = await asyncio.create_subprocess_exec(
-            *cmd, preexec_fn=os.setsid
-        )
-        await self.crazyflies[id].wait()
+
+        process = await asyncio.create_subprocess_exec(*cmd, preexec_fn=os.setsid)
+
+        self.crazyflies[id] = CrazyflieInstance(process=process)
+        await self.crazyflies[id].process.wait()
 
     def _create_start_command(self, id: int) -> str:
         robot_name = "cf{}_ros_ctrl".format(id)
@@ -118,7 +154,11 @@ class Gateway(Node):
     def _add_crazyflie_callback(
         self, req: WebotsCrazyflie.Request, resp: WebotsCrazyflie.Response
     ) -> WebotsCrazyflie.Response:
-        resp.success = self.add_crazyflie(req.id)
+        try:
+            resp.success = self.add_crazyflie(req.id)
+        except (TimeoutError, Gateway) as ex:
+            self._logger.info(str(ex))
+            resp.success = False
         return resp
 
     def _remove_crazyflie_callback(
@@ -128,9 +168,9 @@ class Gateway(Node):
         return resp
 
 
-async def run_node():
+async def run_node(cf_eventloop):
     rclpy.init()
-    gateway = Gateway()
+    gateway = Gateway(cf_eventloop)
     try:
         while rclpy.ok():
             await asyncio.sleep(0.01)
@@ -140,16 +180,59 @@ async def run_node():
         gateway.remove_all_crazyflies()
 
 
+def run_crazyflie_loop(event_loop: asyncio.AbstractEventLoop):
+    asyncio.set_event_loop(event_loop)
+
+    async def keep_alive():
+        try:
+            while True:
+                await asyncio.sleep(1)  # Keep the loop alive
+        except asyncio.CancelledError:
+            pass
+
+    alive_task = asyncio.ensure_future(keep_alive())
+    # If there is no future in the loop it cannot be stopped...
+
+    event_loop.run_forever()
+    alive_task.cancel()
+    for fut in asyncio.all_tasks(event_loop):
+        event_loop.run_until_complete(fut)
+    event_loop.close()
+
+
+def run_gateway_loop(
+    event_loop: asyncio.AbstractEventLoop,
+    crazyflie_event_loop: asyncio.AbstractEventLoop,
+):
+    asyncio.set_event_loop(event_loop)
+    node = asyncio.ensure_future(run_node(crazyflie_event_loop), loop=event_loop)
+    event_loop.run_forever()
+    node.cancel()
+    event_loop.run_until_complete(node)
+    event_loop.close()
+
+
 def main():
-    event_loop = asyncio.get_event_loop()
-    node = asyncio.ensure_future(run_node())
+    gateway_event_loop = asyncio.new_event_loop()
+    crazyflie_event_loop = asyncio.new_event_loop()
+
+    crazyflie_thread = Thread(target=run_crazyflie_loop, args=(crazyflie_event_loop,))
+    gateway_thread = Thread(
+        target=run_gateway_loop, args=(gateway_event_loop, crazyflie_event_loop)
+    )
+    crazyflie_thread.start()
+    gateway_thread.start()
+
     try:
-        event_loop.run_forever()
+        while True:
+            time.sleep(0.5)
     except KeyboardInterrupt:
-        node.cancel()
-        event_loop.run_until_complete(node)
-    finally:
-        event_loop.close()
+        crazyflie_event_loop.stop()
+        gateway_event_loop.stop()
+
+    crazyflie_thread.join()
+    gateway_thread.join()
+    exit()
 
 
 if __name__ == "__main__":
